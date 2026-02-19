07a98593704517d79a02843d71da76c506907f85
masterha@sheridancollege.ca <123147147+MasterHasan095@users.noreply.github.com>
2026-02-19 13:08:33 -0500

    hmm

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// CHANGE THIS to your repo name:
+const repoName = "dj-choppercity";
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
 });
